be482108c30a402c573ce80028eb5ee6bd6d9877
Ismaeil <ismaeilsaberi@gmail.com>
2024-11-05 11:17:51 -0800

    simple change in header direction

--- components/header.tsx
+++ components/header.tsx
@@ -13,7 +13,7 @@ export function Header() {
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 items-center">
         <NavigationMenu>
-          <NavigationMenuList className="flex gap-6">
+          <NavigationMenuList className="flex gap-6 directionrtl">
             <NavigationMenuItem>
               <Link href="/" legacyBehavior passHref>
                 <NavigationMenuLink className="text-sm font-medium">
